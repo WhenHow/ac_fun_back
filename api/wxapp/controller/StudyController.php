590a47f933370f4034b154d25f72de35e85c4d20
xiangwenhao <sherlock3@163.com>
2018-12-20 07:51:23 +0800

    initial

--- api/wxapp/controller/StudyController.php
+++ api/wxapp/controller/StudyController.php
@@ -9,18 +9,30 @@
 namespace api\wxapp\controller;
 
 
+use api\common\logic\UserWordLogic;
+use api\common\map\ErrorCodeMap;
 use think\Request;
 
 class StudyController extends BaseController
 {
+    private $user_id;
+
+
     public function __construct(Request $request = null)
     {
         parent::__construct($request);
+        //检查token
+        $this->checkHeader();
+        $this->user_id = $this->checkToken();
+
     }
 
     public function remember(){
         $book_id = $this->request->param('book_id',1);
         $word_count = $this->request->param('count',10);
-        $user_id = $this->
+
+        $word_logic = new UserWordLogic();
+        $word_list = $word_logic->getUserTodayWords($this->user_id,$book_id,$word_count);
+        return setReturnData(ErrorCodeMap::SUCCESS,'',$word_list);
     }
 }
